d619bbd02183e6f257939c80ea584accc07a209b
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2021-12-23 02:43:28 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'ARP Bike-Rental'
 description 'Rent a bike to make your way faster to you location!'
 author 'hoaaiww'
-version 'v1.2'
+version 'v1.3'
 
 server_scripts {
   'config.lua',
