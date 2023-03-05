4057828e6e2afba81ee54028924a0ef303850e28
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2023-03-05 20:38:30 +0100

    Create locale.lua

---
+++ locale.lua
@@ -0,0 +1,19 @@
+Locales['main'] = {
+    ['press_open_rental'] = 'Press ~INPUT_CONTEXT~ to ~b~rent~s~ a ~y~Bicycle~s~',
+    ['press_open_shop']   = 'Press ~INPUT_CONTEXT~ to ~b~visit~s~ the ~y~Bicycle Shop~s~',
+
+    ['time_left'] = 'Remaining time from the bicycle rental: ~b~',
+
+    ['no_money'] = 'You ~r~do not~s~ have enough money to %s this bicycle. You need ~y~%s~s~ more.',
+    
+    -- Humanizer
+    ['rent'] = 'rent',
+    ['buy'] = 'purchase',
+
+    ['hour'] = 'Hour',
+    ['hours'] = 'Hours',
+    ['minute'] = 'Minute',
+    ['minutes'] = 'Minutes',
+    ['second'] = 'Second',
+    ['seconds'] = 'Seconds',
+}
