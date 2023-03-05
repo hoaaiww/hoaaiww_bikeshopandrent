33bbefbbbac2ad1e3efb9567a4fdc419e0bf69b9
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2023-03-05 14:18:49 +0100

    Updated and Simplified

--- config.lua
+++ config.lua
@@ -1,18 +1,16 @@
-Config = {} -- Do not touch this!
+Config = {}
+Config.CheckForUpdates  = true -- /!\ Recommended 
+Config.Locale = GetConvar('esx:locale', 'main') -- Translate your language in the "locale.lua"
 
 Config.RentPlaces = {
-    Places = {
-        Rental = { -- The positions of where can players rent bike. "h" is the npc's heading.
-            { x = 221.25,    y = -857.07,  z = 30.20, h = 0.0 }, -- Main Public Garage
-            { x = -520.96,   y = -261.95,  z = 35.50, h = 206.43 }, -- Municipality House
-            { x = 301.13,    y = -1393.93, z = 31.22, h = 0.0 }, -- Los Santos Central Hospital
-            { x = 1854.07,   y = 3752.85,  z = 33.12, h = 149.38 }, -- Sandy Shores
-            { x = -93.23,    y = 6325.83,  z = 31.49, h = 268.18 }, -- Paleto Bay
-            { x = -2561.73,  y = 2317.22,  z = 33.21, h = 0.0}, -- At the west mid-highway
-            { x = -1261.66,  y = -1439.61, z = 4.35,  h = 99.73}, -- Near the beach
-            { x = -1050.69,  y = -2723.78, z = 13.75, h = 0.0 } -- Airport
-        }
-    }
+    { x = 221.25,    y = -857.07,  z = 30.20 }, -- Main Public Garage
+    { x = -520.96,   y = -261.95,  z = 35.50 }, -- Municipality House
+    { x = 301.13,    y = -1393.93, z = 31.22 }, -- Los Santos Central Hospital
+    { x = 1854.07,   y = 3752.85,  z = 33.12 }, -- Sandy Shores
+    { x = -93.23,    y = 6325.83,  z = 31.49 }, -- Paleto Bay
+    { x = -2561.73,  y = 2317.22,  z = 33.21 }, -- At the west mid-highway
+    { x = -1261.66,  y = -1439.61, z = 4.350 }, -- Near the beach
+    { x = -1050.69,  y = -2723.78, z = 13.75 }  -- Airport
 }
 
 Config.NPC = { -- The NPC at the bicycle shop.
@@ -20,7 +18,10 @@ Config.NPC = { -- The NPC at the bicycle shop.
     hash = 0x040EABE3 -- NPC's hash. It is under the model name. 
 }
 
-Config.BicycleShop = { x = -1223.08, y = -1495.86, z = 4.35, h = 96.25 } -- "h" is for the npc's heading.
+Config.BicycleShop = { 
+    coords    = { x = -1223.08, y = -1495.86, z = 4.35, h = 96.25 }, -- "h" is for the npc's heading.
+    bikeSpawn = { x = -1224.74, y = -1493.78, z = 3.79, h = 107.83 } -- "h" is for the spawn heading.
+}
 
 Config.Currency = '$' -- You can set the currency whatever you want. Ect.: $, € or £
 Config.CurrencyPlacement = 'before' -- currency placement 'before' or 'after' the prices (ex.: before: $10 or after: 10$)
@@ -60,7 +61,7 @@ Config.Bicycles = { -- bicycles' name rent price/minute and the price in case if
         name      = 'Tri-Cycles Race Bicycle',
         rentPrice = 10,
         buyPrice  = 1000
-    },
+    }
 }
 
 Config.NotificationSettings = {
@@ -83,27 +84,31 @@ Config.NotificationSettings = {
     }
 }
 
-Config.BlipAndMarker = {
-    blipName    = 'City Bicycle Rental',
-    blipColour  = 66,  -- The blip color. - You can find more here: https://docs.fivem.net/docs/game-references/blips/#blip-colors
-    blipId      = 226,  -- The blip icon. - You can find more here: https://docs.fivem.net/docs/game-references/blips/
-    blipSize    = 1.0,  -- The blip size.
+Config.BlipAndMarkers = {
+    -- Markers
+    Markers = {
+        Distance  = 20, -- The distance where inside the value (in meters) players can see the marker
+        Type      = 21, -- The marker type. - You can find more here: https://docs.fivem.net/docs/game-references/markers/
+        Size      = { x = 0.4, y = 0.4, z = 0.4}, -- The size of the marker
+        Colour    = { r = 240, g = 203, b = 87, a = 150}, -- The color of the marker
+        Bounce   = false, -- Make the marker bouncing
+        Rotate    = true, -- Make the marker rotating 360°
+        Facing    = false -- Make the marker always face to you (It works only if the markerRotate is set on false)
+    },
 
-    -- Rent Markers
-    markerDistance  = 20, -- The distance where inside the value (in meters) players can see the marker
-    markerType      = 21, -- The marker type. - You can find more here: https://docs.fivem.net/docs/game-references/markers/
-    markerSize      = { x = 0.4, y = 0.4, z = 0.4}, -- The size of the marker
-    markerColour    = { r = 240, g = 203, b = 87, a = 150}, -- The color of the marker
-    markerBounce   = false, -- Make the marker bouncing
-    markerRotate    = true, -- Make the marker rotating 360°
-    markerFacing    = false, -- Make the marker always face to you (It works only if the markerRotate is false
+    -- Rental blip
+    Rental = {
+        blipName    = 'City Bicycle Rental',
+        blipColour  = 66,  -- The blip color. - You can find more here: https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        blipId      = 226,  -- The blip icon. - You can find more here: https://docs.fivem.net/docs/game-references/blips/
+        blipSize    = 1.0  -- The blip size.
+    },
 
-    -- bike shop blip details
-    shopblipName    = 'City Bicycle Shop',
-    shopblipColour  = 47,
-    shopblipId      = 226,
-    shopblipSize    = 1.2,
+    -- Shop blip
+    Shop = {
+        blipName    = 'City Bicycle Shop',
+        blipColour  = 47,
+        blipId      = 226,
+        blipSize    = 1.2
+    }
 }
-
-Config.checkForUpdates  = true -- Check for updates? /!\ Recommended
-Config.Version          = '2.2' -- Dont edit!
