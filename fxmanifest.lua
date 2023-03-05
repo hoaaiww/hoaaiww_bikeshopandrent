6bfb40ea6bc27b05f8c0b4d1eb61ec58bd97b6ff
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2023-03-05 14:17:58 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,28 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 name 'ARP Bicycle rental and shop'
 description 'Rent or buy a bike to make your way faster to you location or just for fun!!'
 author 'hoaaiww'
-version '2.2'
+version '2.3'
 
-server_scripts {
-  '@oxmysql/lib/MySQL.lua',
+shared_scripts {
+  '@es_extended/imports.lua',
+  '@es_extended/locale.lua',
+  'locale.lua',
   'config.lua',
-  'server/server.lua'
 }
 
 client_scripts {
-  'config.lua',
-  'client/client.lua'
+  'client/functions.lua',
+  'client/main.lua'
+}
+
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'server/updater.lua',
+  'server/main.lua'
 }
 
 files {
@@ -28,6 +36,5 @@ ui_page 'html/ui.html'
 
 dependencies {
   'oxmysql',
-  'es_extended',
-  'esx_vehicleshop'
+  'es_extended'
 }
